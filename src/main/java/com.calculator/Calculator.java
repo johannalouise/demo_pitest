8a2653386d668239a1e81ac27bc407c6eff4f725
Lizzie Queen <louiseze@kth.se>
2020-05-03 13:23:41 +0200

    feature/test&function: added more tests and functions

--- src/main/java/com.calculator/Calculator.java
+++ src/main/java/com.calculator/Calculator.java
@@ -7,4 +7,10 @@ public class Calculator {
     public int addition (int a, int b){
         return a+b;
     }
+
+    public int subtraction (int a, int b) { return a-b;}
+
+    public boolean isPositive (int a) {return a >= 0;}
+
+    public boolean isNegative (int a) {return a < 0;}
 }
--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -1,17 +1,81 @@
 package com.calculator;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    public Calculator calculator = new Calculator();
+
     @Test
     public void addition() {
+        // GIVEN
         int a = 1;
         int b = 2;
 
-        Calculator calculator = new Calculator();
+        // WHEN
         int result = calculator.addition(a,b);
-        assertEquals(3, result);
+
+        // THEN
+        assertEquals("1 + 2 = 3",3, result);
     }
+
+    @Test
+    public void subtraction() {
+        // GIVEN
+        int a = 2;
+        int b = 1;
+
+        // WHEN
+        int result = calculator.subtraction(a,b);
+
+        // THEN
+        assertEquals("2 - 1 = 1", 1, result);
+    }
+
+    @Test
+    public void isPositive() {
+        // GIVEN
+        int a = 2;
+
+        // WHEN
+        boolean resultTrue = calculator.isPositive(a);
+
+        // THEN
+        assertTrue("2 is a positive number", resultTrue);
+
+        // GIVEN
+        int b = -2;
+
+        // WHEN
+        boolean resultFalse = calculator.isPositive(b);
+
+        // THEN
+        assertFalse("-2 is not a positive number", resultFalse);
+
+    }
+
+    @Test
+    public void isNegative() {
+        // GIVEN
+        int a = -1;
+
+        // WHEN
+        boolean resultTrue = calculator.isNegative(a);
+
+        // THEN
+        assertTrue("-1 is a negative number", resultTrue);
+
+        // GIVEN
+        int b = 0;
+
+        // WHEN
+        boolean resultFalse = calculator.isNegative(b);
+
+        // THEN
+        assertFalse("0 is not a negative number", resultFalse);
+
+    }
+
 }
