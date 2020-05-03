6e5592da336e5a5533c5cb13d3fe6862a60a7b27
Johanna <jii@kth.se>
2020-05-03 09:59:01 +0200

    refactor/changed the structure of the project

--- src/main/java/com.calculator/Calculator.java
+++ src/main/java/com.calculator/Calculator.java
@@ -1,4 +1,4 @@
-package main;
+package com.calculator;
 
 public class Calculator {
 
--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -1,7 +1,6 @@
-package main;
+package com.calculator;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
