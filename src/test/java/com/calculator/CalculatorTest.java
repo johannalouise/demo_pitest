f28d6979ac01adc291283553cd104b77e020ca35
Lizzie Queen <louiseze@kth.se>
2020-05-04 10:34:47 +0200

    fix/changed tests

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -10,71 +10,29 @@ public class CalculatorTest {
 
     @Test
     public void addition() {
-        // GIVEN
-        int a = 1;
-        int b = 2;
-
-        // WHEN
-        int result = calculator.addition(a,b);
-
-        // THEN
-        assertEquals("1 + 2 = 3",3, result);
+        assertEquals("1 + 2 = 3",3, calculator.addition(1,2));
     }
 
     @Test
     public void subtraction() {
-        // GIVEN
-        int a = 2;
-        int b = 1;
-
-        // WHEN
-        int result = calculator.subtraction(a,b);
-
-        // THEN
-        assertEquals("2 - 1 = 1", 1, result);
+        assertEquals("2 - 1 = 1", 1, calculator.subtraction(2,1));
     }
 
     @Test
     public void isPositive() {
-        // GIVEN
-        int a = 2;
 
-        // WHEN
-        boolean resultTrue = calculator.isPositive(a);
+        assertTrue("0 is a positive number", calculator.isPositive(0));
 
-        // THEN
-        assertTrue("2 is a positive number", resultTrue);
-
-        // GIVEN
-        int b = -2;
-
-        // WHEN
-        boolean resultFalse = calculator.isPositive(b);
-
-        // THEN
-        assertFalse("-2 is not a positive number", resultFalse);
+        assertFalse("-2 is not a positive number", calculator.isPositive(-2));
 
     }
 
     @Test
     public void isNegative() {
-        // GIVEN
-        int a = -1;
 
-        // WHEN
-        boolean resultTrue = calculator.isNegative(a);
+        assertTrue("-1 is a negative number", calculator.isNegative(-1));
 
-        // THEN
-        assertTrue("-1 is a negative number", resultTrue);
-
-        // GIVEN
-        int b = 0;
-
-        // WHEN
-        boolean resultFalse = calculator.isNegative(b);
-
-        // THEN
-        assertFalse("0 is not a negative number", resultFalse);
+        assertFalse("0 is not a negative number", calculator.isNegative(0));
 
     }
 
